50d4aeaaf5b2144de10547a739fb004ad749e754
Garvv235 <67822641+Garvv235@users.noreply.github.com>
2021-06-02 22:39:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var dog, happyDog,dogImg, database, foodS, foodStock
+
+function preload()
+{
+ dogImg = loadImage("sprites/Dog.png");
+ happyDog = loadImage("sprites/happydog.png");
+}
+
+function setup() {
+	database = firebase.database();
+  
+  createCanvas(500, 500);
+  
+  var dog = createSprite(250,300,150,150);
+  dog.addImage(dogImg);
+  dog.scale = 0.15;
+  
+  foodStockRef = database.ref('Food');
+  foodStockRef.on("value",readStock);
+
+}
+
+
+function draw() {  
+  background(46, 139, 87);
+
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(happyDog);
+  }
+
+  drawSprites();
+  fill(255,255,254);
+  stroke("white");
+  text("Food left :"+foodS,170,200);
+  textSize(13);
+  text("Note : Press Up Arrow key to feed milk to John",130,300);
+
+
+}
+function readStock(data){
+  foodS = data.val();
+  }
+  
+  function writeStock(x){
+    if(x <= 0){
+      x = 0;
+    }
+    else{
+      x = x-1;
+    }
+      database.ref("/").update({
+        Food : x,
+      })
+    }
+
+
+
